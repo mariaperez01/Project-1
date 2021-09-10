fbfa58e30ca1ff14ab6136e1f3f53abdd080e5c7
elenapuj <89639906+elenapuj@users.noreply.github.com>
2021-09-10 11:02:09 +0200

    Update Problem7.cpp

--- Problem7.cpp
+++ Problem7.cpp
@@ -11,10 +11,17 @@ using namespace std;
 
 
 int main() {
-
+    
+    //First, you have to introduce n, the number of grid points in which you want to divide the interval [0,1]. Here, we have used n=100 as an example 
+   
     int n = 100;
 
+    //Now we introduce m to make the writing easier later
+    
     int m = n-1;
+    
+    
+   //We introduce the vectors a, b and c that will always be the same 
 
     vec a = vec(m, fill::zeros);
     vec b = vec(n, fill::zeros);
@@ -24,18 +31,21 @@ int main() {
     b.fill(2);
     c.fill(-1);
 
-    a.brief_print("a.");
-    b.brief_print("b.");
-    c.brief_print("c.");
-
+    //Now, we create the vector x, a vector of n points starting on 0 and ending on 1
 
     vec x = linspace<vec>(0, 1, n);
+    
+    //With it, we compute f
+    
     vec f = 100. * exp(-10. * x);
-
+    
+    /After that, we calculate the stepsize h, and with that and f, we calculate the vector g
+    
     double h = 1 / (1.0*n);
     vec g = f*pow(h,2);
-    g.brief_print("g:");
 
+    
+    //We now do the forward calculations
 
     for (int i = 0; i < m; i++) {
         
@@ -44,17 +54,15 @@ int main() {
 
     }
 
-    //Now we will get the values for the vector we were looking for, that we will call v. First, we will create a zero vector an later we will fill it with the results we get
-
+    //And, finally, we will get the values for the vector we were looking for, that we will call v, using the backwards calculations 
+    
     vec v = vec(n, fill::zeros);
 
-    //We won't change the first and last element to satisfy the boundary conditions
-
-    //Then we get the next-to-last element of the vector, which is the only one that's obtained in a different way
+    //We won't change the first and last element to satisfy the boundary conditions, so we get the next-to-last element of the vector, which is the only one that's obtained in a different way
 
     v(m - 1) = g(m - 1) / b(m - 1);
 
-    //Now we get the rest of them using a for
+    //Now we get the rest of them using a for(except for the first one, as we have already said)
 
     for (int i = 0; i < m-1; i++) {
 
