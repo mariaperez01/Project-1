05659dc3a63accd9be0345fc276b7154c89cf58b
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-09-10 18:13:24 +0200

    Update Problem8a(graphics).py

--- Problem8a(graphics).py
+++ Problem8a(graphics).py
@@ -42,9 +42,11 @@ for i in range(0,5):
     rel_err = np.absolute(abs_err / u(x))
     log10_abs_err = np.log10(abs_err)
     log10_rel_err = np.log10(rel_err)
+    log10_abs_err[0] = -20
     plt.plot(x, log10_abs_err, label="n="+nseq[i])
     
 plt.title("Log10 of absolute error", fontsize=10)
+plt.ylim(-14,-1)
 plt.ylabel(r'$log_{10}(\Delta_i)$')
 plt.xlabel("x")
 plt.grid(True) #Grids get painted
