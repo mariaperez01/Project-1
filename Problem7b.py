69dbf9e07d86fa4f6c87d67f049091cf192e31e2
JulianElToro <89639454+JulianElToro@users.noreply.github.com>
2021-09-10 14:00:58 +0200

    Update Problem7b.py

--- Problem7b.py
+++ Problem7b.py
@@ -2,10 +2,12 @@
 """
 Created on Wed Sep  8 20:55:12 2021
 
-@author: julia
+@author: JulianElToro
 """
 import matplotlib.pyplot as plt
 import numpy as np
+
+# We will create some strings for some steps in the loops with a different n
 
 textnames = ['vx10solution.txt' , 'vx100solution.txt' , 'vx1000solution.txt' , 
              
@@ -16,15 +18,15 @@ nseq = ['10' , '100', '1000' , '10000' , '100000' , '1000000']
 Problem = ["Problem7bn10.pdf","Problem7bn100.pdf","Problem7bn1000.pdf",
            "Problem7bn10000.pdf","Problem7bn100000.pdf","Problem7bn1000000.pdf",]
 
+# The loop
 
 for i in range(0,6): 
 
-    v = [] #Vector x is created
+    v = [] #Vector v is created
 
     x = [] #Vector x is  created 
 
-    '''The  code  opens  the  data  file and , skipping  the  first line , 
-    adds  every xvalue  to the x vector. The  same  operation  is done  with  the v values.'''
+    #The  code  opens  the  data  file and adds every xvalue to the x vector. and the same with the v values
 
     with  open(textnames[i] , "r") as  infile:
         infile.readline () #Skip  first  line.
@@ -32,21 +34,22 @@ for i in range(0,6):
         for  line in  lines:
             vals = line.split() #Splits  the  data  file  into  two  columnes
             x.append(float(vals [0])) #Adds  every x value  to  vector x
-            v.append(float(vals [1])) #Adds  every u(x) value  to  vector v
+            v.append(float(vals [1])) #Adds  every v value  to  vector v
             
        # Exact solution
         u = 1-(1-np.exp(-10))*np.array(x)-(np.exp(-10*np.array(x)))
-
+        
+    #Plot
     plt.figure()
-    plt.xlim(0,1)
-    plt.plot(x , u , color='red' , label="u(x)")
-    plt.plot(x , v , color = 'blue', label="v" )
-    plt.ylabel('u(x) | v')
+    plt.xlim(0,1) # x-axis limit
+    plt.plot(x , u , color='red' , label="u(x)") #Plot of the exact solution
+    plt.plot(x , v , color = 'blue', label="v" ) #Plot of the v values
+    plt.ylabel('u(x) | v') #
     plt.xlabel('x')
     plt.title('Exact solution u(x) vs solution v, n = ' +str(nseq[i]))
-    plt.grid(True)
-    plt.legend(loc="upper right")
-    plt.savefig(Problem[i])
+    plt.grid(True) #A grid is plotted
+    plt.legend(loc="upper right") #Legend in the topright corner
+    plt.savefig(Problem[i]) #We save the plot into a .pdf file
     plt.show
 
 
