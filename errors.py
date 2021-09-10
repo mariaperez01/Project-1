2ce6b0a959157947c8ca21a0a952a5324f9e2ba7
ivanvillegas7 <89591182+ivanvillegas7@users.noreply.github.com>
2021-09-10 12:39:14 +0200

    Update errors.py

--- errors.py
+++ errors.py
@@ -2,89 +2,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+n=100
+
+def u(x: float):
+    return 1-(1-np.exp(-10))*x-np.exp(-10*x)
+    
 # Read data into numpy arrays
 
+infilename = "vx100solution.txt"
 
-h, d2u_approx, d2u_exact = np.loadtxt("output.txt", usecols=(0,1,2), unpack=True)
+xl = []
+
+vl = []
+
+with open(infilename , "r") as infile:
+    infile.readline()
+    lines= infile.readlines()
+    for line in lines:
+        vals = line.split()
+        xl.append(float(vals[0]))
+        vl.append(float(vals[1]))
+        
+# h, d2u_approx, d2u_exact = np.loadtxt("output.txt", usecols=(0,1,2), unpack=True)
+
+x = np.array(xl)
+
+v = np.array(vl)
 
 # Compute numpy arrays with the absolute error, relative error and log10(relative error)
-abs_err = np.absolute(d2u_approx - d2u_exact)
-rel_err = np.absolute(abs_err / d2u_exact)
+abs_err = np.absolute(v - u(x))
+rel_err = np.absolute(abs_err / u(x))
+log10_abs_err = np.log10(abs_err)
 log10_rel_err = np.log10(rel_err)
 
-# Array with log10(h)
-log10_h = np.log10(h)
-
 # Make plots
-
-# Common title string
-exact_result = d2u_exact[0]
-common_title_string = "FOR APPROXIMATION OF\n u''(x=0.5) = -100*exp(-10*0.5) = -0.6737947".format(exact_result)
 
 # Figure size (inches)
 figwidth = 5.5
 figheight = figwidth / 1.33333
 
-# Plot absolute error vs h
-plt.figure(figsize=(figwidth, figheight))
-plt.plot(h, abs_err, '--', c="0.8", linewidth=1.5)
-plt.plot(h, abs_err, '.', c="black", markersize=10)
-plt.title("ABSOLUTE ERROR " + common_title_string, fontsize=10)
-plt.ylabel("Absolute error")
-plt.xlabel("h")
-plt.grid(True) #Grids get painted
-plt.savefig("abs_err_vs_h.pdf")
-
 # Plot absolute error vs log10(h)
 plt.figure(figsize=(figwidth, figheight))
-plt.plot(log10_h, abs_err, '--', c="0.8", linewidth=1.5)
-plt.plot(log10_h, abs_err, '.', c="black", markersize=10)
-plt.title("ABSOLUTE ERROR " + common_title_string, fontsize=10)
-plt.ylabel("Absolute error")
-plt.xlabel("Log10(h)")
+plt.plot(x, log10_abs_err)
+plt.title("Log10 of absolute error n=100", fontsize=10)
+plt.ylabel(r'$log_{10}(\Delta_i)$')
+plt.xlabel("x")
 plt.grid(True) #Grids get painted
-plt.savefig("abs_err_vs_log10_h.pdf")
-
-# Plot absolute error vs h on log-log axes
-plt.figure(figsize=(figwidth, figheight))
-plt.plot(h, abs_err, '--', c="0.8", linewidth=1.5)
-plt.plot(h, abs_err, '.', c="black", markersize=10)
-plt.title("ABSOLUTE ERROR " + common_title_string, fontsize=10)
-plt.ylabel("Absolute error")
-plt.xlabel("h")
-plt.xscale("log")
-plt.yscale("log")
-plt.grid(True) #Grids get painted
-plt.savefig("abs_err_vs_h_loglog.pdf")
-
-# Plot relative error vs log10(h)
-plt.figure(figsize=(figwidth, figheight))
-plt.plot(log10_h, rel_err, '--', c="0.8", linewidth=1.5)
-plt.plot(log10_h, rel_err, '.', c="black", markersize=10)
-plt.title("RELATIVE ERROR " + common_title_string, fontsize=10)
-plt.ylabel("Relative error")
-plt.xlabel("Log10(h)")
-plt.grid(True) #Grids get painted
-plt.savefig("rel_err_vs_log10_h.pdf")
-
-# Plot log10(relative error) vs log10(h)
-plt.figure(figsize=(figwidth, figheight))
-plt.plot(log10_h, log10_rel_err, '--', c="0.8", linewidth=1.5)
-plt.plot(log10_h, log10_rel_err, '.', c="black", markersize=10)
-plt.title("LOG10(RELATIVE ERROR) " + common_title_string, fontsize=10)
-plt.ylabel("Log10(Relative error)")
-plt.xlabel("Log10(h)")
-plt.grid(True) #Grids get painted
-plt.savefig("log10_rel_err_vs_log10_h.pdf")
-
-# Plot relative error vs h on log-log axes
-plt.figure(figsize=(figwidth, figheight))
-plt.plot(h, rel_err, '--', c="0.8", linewidth=1.5)
-plt.plot(h, rel_err, '.', c="black", markersize=10)
-plt.title("RELATIVE ERROR " + common_title_string, fontsize=10)
-plt.ylabel("Relative error")
-plt.xlabel("h")
-plt.xscale("log")
-plt.yscale("log")
-plt.grid(True) #Grids get painted
-plt.savefig("rel_err_vs_h_loglog.pdf")
+plt.savefig("Problem_8-a_plot_n-100.pdf")
